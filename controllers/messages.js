a21474d9509961fa40e24b6264406a93fe59ca99
oscar120020 <omartinez@intellisys.com.do>
2022-03-01 16:58:11 -0400

    adaptandose a app en angular

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,22 +1,32 @@
 const Messages = require("../models/messageModel")
+const User = require("../models/userModel")
 
 const getMessages = async(req, res) => {
     const myId = req.uid
     const {from} = req.params
 
     try {
-        const last20Msg = await Messages.find({
+        // Get messages
+        let last20Msg = await Messages.find({
             $or:[
                 {to: myId, from},
                 {to: from, from: myId}
             ]
         })
-        .sort({createdAt: "asc"})
-        .limit(35)    
+        .sort({createdAt: "desc"})
+        .limit(35)
+        
+        // Get user
+        const user = await User.findById(from)
 
+        // Order messages
+        last20Msg = last20Msg.sort((a, b) => a.createdAt - b.createdAt)
+
+        // Response
         res.json({
             ok: true,
-            messages: last20Msg
+            messages: last20Msg,
+            user
         })
     } catch (error) {
         return res.status(400).json({
--- models/sockets.js
+++ models/sockets.js
@@ -4,36 +4,39 @@ const { validateJWT } = require("../helpers/jwt");
 class Sockets {
     constructor( io ){
         this.io = io;
-
         this.socketsEvents()
     }
 
     socketsEvents(){
-        this.io.on('connection', async( socket ) => {
-            const [valid, uid] = validateJWT(socket.handshake.query["x-token"])
-            if(!valid){
-                console.log("socket no valid");
-                return socket.disconnect()
-            }
-            await userConnected(uid)
-            socket.join(uid)
-
-            socket.on("inbox-message", async(payload) => {
-                const message = await saveMessages(payload)
-                this.io.to(payload.to).emit("inbox-message", message)
-                this.io.to(payload.from).emit("inbox-message", message)
-            })
-
-            socket.on("writing", ({to, from, writing}) => {
-                this.io.to(to).emit("writing", {writing, from})
-            })
-
-            socket.on("disconnect", async() => {
-                await userDisconnected(uid)
+        this.io.on('connection', ( socket ) => {
+            socket.on("main-connection", async(token) => {
+                const [valid, uid] = validateJWT(token)
+                if(!valid){
+                    console.log("socket no valid");
+                    return socket.disconnect()
+                }
+                console.log("////////////usuario conectado");
+                await userConnected(uid)
+                socket.join(uid)
+    
+                socket.on("inbox-message", async(payload) => {
+                    const message = await saveMessages(payload)
+                    this.io.to(payload.to).emit("inbox-message", message)
+                    this.io.to(payload.from).emit("inbox-message", message)
+                })
+    
+                socket.on("writing", ({to, from, writing}) => {
+                    this.io.to(to).emit("writing", {writing, from})
+                })
+    
+                socket.on("disconnect", async() => {
+                    await userDisconnected(uid)
+                    this.io.emit("user-list", await getAllUsers(uid))
+                    console.log("***********usuario desconectado");
+                })
+    
                 this.io.emit("user-list", await getAllUsers(uid))
             })
-
-            this.io.emit("user-list", await getAllUsers(uid))
         });
     }
 }
